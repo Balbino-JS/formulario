786591c42fae0d5cfcb3e3815b88171365c3d62d
Balbino-teste <balbinojteste@gmail.com>
2025-07-02 21:25:31 -0300

    Adicionei o código push e o delet

--- Server/server.js
+++ Server/server.js
@@ -1,6 +1,7 @@
 import express from 'express';
-import pkg from './generated/prisma/index.js'; // caminho correto para o Prisma Client gerado
-import { ObjectId } from 'bson';
+import pkg from './generated/prisma/index.js';
+import { ObjectId } from 'bson'; 
+
 
 const { PrismaClient } = pkg;
 
@@ -36,6 +37,41 @@ app.get('/usuarios', async (req, res) => {
     } catch (error) {
         res.status(500).json({ error: 'Erro ao buscar usuários.', detalhes: error.message })
     }
+})
+
+// auteraça~de usuários
+
+app.put('/usuarios/:id', async (req, res) => {
+    try {
+        const user = await prisma.user.update({
+            where:{
+                id: req.params.id
+            },
+            data: {
+                name: req.body.name,
+                testimony: req.body.testimony
+            }
+        })
+
+        res.status(201).json(user);
+    } catch (error) {
+        res.status(500).json({ error: 'Erro ao salvar usuário.', detalhes: error.message });
+    }
+})
+
+app.delete('/usuarios/:id', async (req, res) => {
+    try {
+        const id = req.params.id;
+        await prisma.user.delete({
+            where: {
+                id: new ObjectId(id)
+            }
+        });
+        res.status(200).json({ message: 'Usuário deletado com sucesso!' });
+    } catch (error) {
+        console.error('Erro ao deletar usuário:', error);
+        res.status(500).json({ error: 'Erro ao deletar usuário.', detalhes: error.message });
+    }
 })
 
 
